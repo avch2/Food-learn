50ca49b8262a1cd686f9105d74141e8d4adc6c76
avch <avch2@ukr.net>
2023-06-20 13:46:19 +0300

    navig for slide

--- js/script.js
+++ js/script.js
@@ -317,6 +317,7 @@ function showThanksModal(message) { // скрыть модуль диалога 
 // Sliders
 
 const slides = document.querySelectorAll('.offer__slide'),
+    slider = document.querySelector('.offer__slider'),
     prev = document.querySelector('.offer__slider-prev'),
     next = document.querySelector('.offer__slider-next');
     total = document.querySelector('#total'),
@@ -346,6 +347,51 @@ slides.forEach(slide => {
     slide.style.width = width;
 });
 
+slider.style.position = 'relative';
+
+const indicators = document.createElement('ol'),
+    dots = [];
+indicators.classList.add('carusel-indicators');
+indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+`;
+slider.append(indicators);
+
+for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+    `;
+    if (i == 0) {
+        dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+}
+
+
 next.addEventListener('click', () => {
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
         offset = 0;
@@ -366,6 +412,9 @@ next.addEventListener('click', () => {
     } else {
         current.textContent = slideIndex;
     }
+// подсвечиваем маркеры кадров слайдера
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
 });
 
 prev.addEventListener('click', () => {
@@ -389,7 +438,29 @@ prev.addEventListener('click', () => {
     } else {
         current.textContent = slideIndex;
     }
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
 });
+// навигация по маркепам слайдера
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to')
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if (slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = 1;
+        });
+    });
 
     // showSlides(slideIndex);
 
